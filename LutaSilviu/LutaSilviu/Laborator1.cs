a6ebd996123ebcfaa118f981b1cbbd830eeab857
Vasile <silviu.luta@student.usv.ro>
2023-10-26 14:15:50 +0300

    added movement with a,d,s,w

--- LutaSilviu/LutaSilviu/Laborator1.cs
+++ LutaSilviu/LutaSilviu/Laborator1.cs
@@ -9,16 +9,37 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+
+
+/*
+* Luta Silviu Vasilica,3131b
+*
+* 22.10.2023
+*
+* Se rezolva laboratorul 2
+* se creeaza un cub,si se controleaza cu ajutorul tastelor a,d,w,s
+*
+* daca apesi pe butonul stanga al mouse vei controla cu mouse
+*
+ */
+
 
 namespace EGC
 {
     internal class Laborator1 : GameWindow
     {
         private float x = 1.0f;
+        private float y = 1.0f;
         public Laborator1() : base(800, 600, new GraphicsMode(32, 24, 0, 8))
         {
             VSync = VSyncMode.On;
+
         }
+
+
 
         static void Main(string[] args)
         {
@@ -50,6 +71,16 @@ namespace EGC
         protected override void OnMouseMove(MouseMoveEventArgs e)
         {
             base.OnMouseMove(e);
+            MouseState mouseState = Mouse.GetState();
+            if (mouseState[MouseButton.Left])
+                if (mouseState.X != e.X || mouseState.Y != e.Y)
+                {
+
+                    x = (2.0f * mouseState.X / Width) - 1.0f;
+                    y = 1.0f - (2.0f * mouseState.Y / Height);
+
+
+                }
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
@@ -85,14 +116,33 @@ namespace EGC
         {
             base.OnUpdateFrame(e);
             KeyboardState keyboard = Keyboard.GetState();
+            //Verificam ce tasta am apasat,si vom modifica x in functie de nevoie
             if (keyboard[Key.D])
             {
                 x++;
+
             }
             if (keyboard[Key.A])
             {
                 x--;
             }
+
+            if (keyboard[Key.W])
+            {
+                y++;
+            }
+            if (keyboard[Key.S])
+            {
+                y--;
+            }
+            Console.WriteLine(x);
+
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            //  Console.WriteLine(e.X);
         }
 
         private void DrawCube()
@@ -100,44 +150,45 @@ namespace EGC
             GL.Begin(PrimitiveType.Quads);
 
             GL.Color3(Color.Silver);
-            GL.Vertex3(-x, -1.0f, -1.0f);
-            GL.Vertex3(-x, 1.0f, -1.0f);
-            GL.Vertex3(x, 1.0f, -1.0f);
-            GL.Vertex3(x, -1.0f, -1.0f);
+            GL.Vertex3(x - 2, y - 2, -1.0f);
+            GL.Vertex3(x - 2, y, -1.0f);
+            GL.Vertex3(x, y, -1.0f);
+            GL.Vertex3(x, y - 2, -1.0f);
+            /*
+                        GL.Color3(Color.Honeydew);
+                        GL.Vertex3(x, -1.0f, -1.0f);
+                        GL.Vertex3(x, -1.0f, -1.0f);
+                        GL.Vertex3(x, -1.0f, 1.0f);
+                        GL.Vertex3(-x, -1.0f, 1.0f);
 
-            GL.Color3(Color.Honeydew);
-            GL.Vertex3(x, -1.0f, -1.0f);
-            GL.Vertex3(x, -1.0f, -1.0f);
-            GL.Vertex3(x, -1.0f, 1.0f);
-            GL.Vertex3(-x, -1.0f, 1.0f);
+                        GL.Color3(Color.Moccasin);
 
-            GL.Color3(Color.Moccasin);
+                        GL.Vertex3(-x, -1.0f, -1.0f);
+                        GL.Vertex3(-x, -1.0f, 1.0f);
+                        GL.Vertex3(-x, 1.0f, 1.0f);
+                        GL.Vertex3(-x, 1.0f, -1.0f);
 
-            GL.Vertex3(-x, -1.0f, -1.0f);
-            GL.Vertex3(-x, -1.0f, 1.0f);
-            GL.Vertex3(-x, 1.0f, 1.0f);
-            GL.Vertex3(-x, 1.0f, -1.0f);
+                        GL.Color3(Color.IndianRed);
+                        GL.Vertex3(-x, -1.0f, 1.0f);
+                        GL.Vertex3(x, -1.0f, 1.0f);
+                        GL.Vertex3(x, 1.0f, 1.0f);
+                        GL.Vertex3(-x, 1.0f, 1.0f);
 
-            GL.Color3(Color.IndianRed);
-            GL.Vertex3(-x, -1.0f, 1.0f);
-            GL.Vertex3(x, -1.0f, 1.0f);
-            GL.Vertex3(x, 1.0f, 1.0f);
-            GL.Vertex3(-x, 1.0f, 1.0f);
+                        GL.Color3(Color.PaleVioletRed);
+                        GL.Vertex3(-x , 1.0f, -1.0f);
+                        GL.Vertex3(-x, 1.0f, 1.0f);
+                        GL.Vertex3(x, 1.0f, 1.0f);
+                        GL.Vertex3(x, 1.0f, -1.0f);
 
-            GL.Color3(Color.PaleVioletRed);
-            GL.Vertex3(-x, 1.0f, -1.0f);
-            GL.Vertex3(-x, 1.0f, 1.0f);
-            GL.Vertex3(x, 1.0f, 1.0f);
-            GL.Vertex3(x, 1.0f, -1.0f);
-
-            GL.Color3(Color.ForestGreen);
-            GL.Vertex3(x, -1.0f, -1.0f);
-            GL.Vertex3(x, 1.0f, -1.0f);
-            GL.Vertex3(x, 1.0f, 1.0f);
-            GL.Vertex3(x, -1.0f, 1.0f);
-
+                        GL.Color3(Color.ForestGreen);
+                        GL.Vertex3(x, -1.0f, -1.0f);
+                        GL.Vertex3(x, 1.0f, -1.0f);
+                        GL.Vertex3(x, 1.0f, 1.0f);
+                        GL.Vertex3(x, -1.0f, 1.0f);
+                        */
             GL.End();
         }
+
 
     }
 }
